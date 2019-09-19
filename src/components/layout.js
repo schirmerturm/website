43fc5ef3a2153b09a9c67097a88d7eaa3ab04a77
Hannes Hertach <hanneshertach@gmail.com>
2019-09-19 19:27:44 +0200

    Add header image

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import Container from './container'
-import NavBar from './navbar'
+import React from "react"
+import Container from "./container"
+import NavBar from "./navbar"
+import layoutStyles from "./layout.module.css"
+import BackgroundImage from "gatsby-background-image"
+import { StaticQuery, graphql } from "gatsby"
 
 export default ({ children }) => {
-  return <NavBar>
-    <Container>
-      { children }
-    </Container>
-  </NavBar>
+  return (
+    <StaticQuery query={graphql`{
+      file(relativePath: {eq: "pano.jpeg"}) {
+        childImageSharp {
+          fluid(quality: 60) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }`}
+    render={data => (
+      <NavBar>
+        <div className={layoutStyles.headerImage}>
+          <BackgroundImage fluid={data.file.childImageSharp.fluid}>
+            <div className={layoutStyles.fillBackground}></div>
+          </BackgroundImage>
+        </div>
+        <Container>{children}</Container>
+      </NavBar>
+    )}
+  />)
 }
