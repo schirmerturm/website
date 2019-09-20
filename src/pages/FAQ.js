32ac0e080b8a7f2b8ec0e47af6fdb72880ad3d80
Hannes Hertach <hanneshertach@gmail.com>
2019-09-20 11:24:03 +0200

    Add FAQ page

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import FaqQuestion from '../components/faq-question'
 
-export default () => {
-  return (
-    <Layout>
-
-    </Layout>
-  )
+export default ({ data }) => {
+  return <Layout>
+    <h1>FAQs</h1>
+    <h3>Hier findest du eine Sammlung häufig gestellter Fragen</h3>
+    {
+      data.allContentfulFaqFrage.nodes.map((node) => (
+        <FaqQuestion question={node.frage.frage} answer={node.antwort.json}/>
+      ))
+    }
+  </Layout>
 }
+
+export const query = graphql`{
+    allContentfulFaqFrage {
+      nodes {
+        frage {
+          frage
+        }
+        antwort {
+          json
+        }
+      }
+    }
+  }`
