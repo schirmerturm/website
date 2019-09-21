ae37c081ae99d07775893c64228c503b8fa111b5
Hannes Hertach <hanneshertach@gmail.com>
2019-09-21 10:03:09 +0200

    Add restricted google maps api key

--- src/components/anschlag-map.js
+++ src/components/anschlag-map.js
@@ -16,7 +16,7 @@ const Map = withScriptjs(
 
 export default ({ lat, lon }) => (
   <Map
-    googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+    googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyBAD4D95cqaTGqBohvtQMGJsqUSzmyAxGU&v=3.exp&libraries=geometry,drawing,places`}
     loadingElement={<div style={{ height: `100%`, backgroundColor: 'gray' }} />}
     containerElement={<div style={{ height: `400px` }} />}
     mapElement={<div style={{ height: `100%` }} />}
