ad376377c0fedb1053b8d4930be509d6b4e75932
Hannes Hertach <hanneshertach@gmail.com>
2019-09-21 09:54:51 +0200

    Format Anschlag

--- src/templates/stufe-mit-anschlag.js
+++ src/templates/stufe-mit-anschlag.js
@@ -1,44 +1,56 @@
-import React from 'react';
-import Layout from '../components/layout'
-import moment from 'moment'
-import { graphql } from 'gatsby'
-import UeberStufe from '../components/ueber-stufe'
+import React from "react"
+import Layout from "../components/layout"
+import moment from "moment"
+import { graphql } from "gatsby"
+import UeberStufe from "../components/ueber-stufe"
+import AnschlagMap from "../components/anschlag-map"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import stufeStyles from './stufe.module.css'
 
 export default ({ data }) => {
   const stufe = data.contentfulStufe
   const anschlag = data.contentfulStufe.anschlag[0]
 
   function formatTime(time) {
-    const timeFormat = 'llll'
-    return moment(time).locale('de-DE').format(timeFormat)
+    const timeFormat = "dd DD.MM.YYYY hh:mm"
+    return moment(time)
+      .locale("de-DE")
+      .format(timeFormat)
   }
 
   return (
     <Layout>
-      <UeberStufe stufe={stufe}/>
+      <UeberStufe stufe={stufe} />
       <h2>Anschlag</h2>
-      <b>Start:</b> {formatTime(anschlag.startzeit)} <br />
-      <b>Ende:</b> {formatTime(anschlag.endzeit)}
+      <div className={stufeStyles.anschlagBox}>
+        <b>Start:</b> {formatTime(anschlag.startzeit)} bei {anschlag.startort}{" "}
+        <br />
+        <b>Ende:</b> {formatTime(anschlag.endzeit)} bei {anschlag.schlussort}
+        <h4>Infos:</h4>
+        {documentToReactComponents(anschlag.infos.json)}
+      </div>
+      {anschlag.ort != null && (
+        <AnschlagMap lat={anschlag.ort.lat} lon={anschlag.ort.lon} />
+      )}
     </Layout>
   )
 }
 
 export const query = graphql`
   query($slug: String!) {
-    contentfulStufe(fields: {slug: {eq: $slug}}) {
+    contentfulStufe(fields: { slug: { eq: $slug } }) {
       name
       beschreibung {
         json
       }
       anschlag {
-        mitnehmen {
-          json
-        }
         infos {
           json
         }
         endzeit
         startzeit
+        startort
+        schlussort
         ort {
           lat
           lon
