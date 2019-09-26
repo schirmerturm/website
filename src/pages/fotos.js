ddbd2ddc73281a23b42e6983eee248bee5a923b0
Hannes Hertach <hanneshertach@gmail.com>
2019-09-26 11:53:26 +0200

    Add photos page

--- src/pages/fotos.js
+++ src/pages/fotos.js
@@ -1,10 +1,62 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React, { useState } from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import photoStyles from "./fotos.module.css"
+import { MdClose } from 'react-icons/md'
 
-export default () => {
+export default ({ data }) => {
+  const [selectedPicture, setSelectedPicture] = useState(null)
+  const [loading, setLoading] = useState(true)
+
   return (
     <Layout>
-
+      <div className={photoStyles.gridContainer}>
+        {data.allContentfulGallerieBild.nodes.map(node => {
+          return (
+            <div
+              className={photoStyles.gridItem}
+              onClick={() => {
+                setLoading(true)
+                setSelectedPicture(node.bild.file.url)
+              }}
+            >
+              <Img style={{ height: "100%" }} fluid={node.bild.fluid}></Img>
+            </div>
+          )
+        })}
+      </div>
+      {selectedPicture != null && (
+        <div className={photoStyles.dim} onClick={() => setSelectedPicture(null)}>
+          <MdClose className={photoStyles.closeIcon} size='2em' />
+          <div className={photoStyles.pictureContainer}>
+            { loading && <div className={photoStyles.loader}>Loading...</div>}
+            <img
+              style={loading ? { display: "none" } : {}}
+              src={`https:${selectedPicture}`}
+              onLoad={() => setLoading(false)}
+              alt="alt"
+            ></img>
+          </div>
+        </div>
+      )}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allContentfulGallerieBild {
+      nodes {
+        bild {
+          fluid {
+            ...GatsbyContentfulFluid_tracedSVG
+          }
+          file {
+            url
+          }
+        }
+      }
+    }
+  }
+`
