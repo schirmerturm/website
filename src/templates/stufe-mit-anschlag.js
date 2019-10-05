60a8d4034da22fd26e42ccf747368328a82b87e8
Hannes Hertach <hanneshertach@gmail.com>
2019-10-05 13:12:42 +0100

    Fix Anschlag expanding on interaction with google maps

--- src/templates/stufe-mit-anschlag.js
+++ src/templates/stufe-mit-anschlag.js
@@ -6,7 +6,7 @@ import UeberStufe from "../components/ueber-stufe"
 import AnschlagMap from "../components/anschlag-map"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import stufeStyles from "./stufe.module.css"
-import { MdArrowDropDown } from 'react-icons/md'
+import { MdArrowDropDown } from "react-icons/md"
 
 export default ({ data }) => {
   const stufe = data.contentfulStufe
@@ -23,21 +23,37 @@ export default ({ data }) => {
   return (
     <Layout>
       <h1>{stufe.name}</h1>
-      <div className={stufeStyles.anschlagBox} onClick={() => setShowAnschlag(!showAnschlag)}>
-      <div className={stufeStyles.questionContainer}>
-        <h2 className={stufeStyles.anschlagTitle}>Anschlag</h2>
-        <span><MdArrowDropDown size='1.5em'/></span>
-      </div>
-        <div className={showAnschlag ? stufeStyles.expanded : stufeStyles.collapsed}>
-            <b>Start:</b> {formatTime(anschlag.startzeit)} bei{" "}
-            {anschlag.startort} <br />
-            <b>Ende:</b> {formatTime(anschlag.endzeit)} bei{" "}
-            {anschlag.schlussort}
-            <h4>Infos:</h4>
-            {documentToReactComponents(anschlag.infos.json)}
+      <div
+        className={stufeStyles.anschlagBox}
+        onClick={e => {
+          // Prevent box from closing if user interacts with map
+          if (
+            e.target.classList.contains(stufeStyles.anschlagBox) ||
+            e.target.classList.contains(stufeStyles.titleContainer)
+          ) {
+            setShowAnschlag(!showAnschlag)
+          }
+        }}
+      >
+        <div className={stufeStyles.titleContainer}>
+          <h2 className={stufeStyles.anschlagTitle}>Anschlag</h2>
+          <span>
+            <MdArrowDropDown size="1.5em" />
+          </span>
+        </div>
+        <div
+          className={
+            showAnschlag ? stufeStyles.expanded : stufeStyles.collapsed
+          }
+        >
+          <b>Start:</b> {formatTime(anschlag.startzeit)} bei {anschlag.startort}{" "}
+          <br />
+          <b>Ende:</b> {formatTime(anschlag.endzeit)} bei {anschlag.schlussort}
+          <h4>Infos:</h4>
+          {documentToReactComponents(anschlag.infos.json)}
           {anschlag.ort != null && (
             <AnschlagMap lat={anschlag.ort.lat} lon={anschlag.ort.lon} />
-            )}
+          )}
         </div>
       </div>
       <UeberStufe stufe={stufe} />
