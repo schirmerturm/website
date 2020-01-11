91119df33e25a22af7da125dfc94a670e2668f98
Hannes Hertach <hanneshertach@gmail.com>
2020-01-11 12:18:38 +0000

    Improve carousel component

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,31 +1,73 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import carouselStyles from "./carousel.module.css"
 import { MdChevronRight, MdChevronLeft } from "react-icons/md"
 
 export default ({ urls }) => {
   const [currentPic, setCurrentPic] = useState(0)
   const url = urls[currentPic]
+  const [loading, setLoading] = useState(true)
+  const [showCounter, setShowCounter] = useState(true)
 
-  function nextPic() {
-    setCurrentPic((currentPic + 1) % urls.length)
+  const disableClick = urls.length > 1 ? "" : carouselStyles.singleImage
+
+  // Preload all images
+  urls.forEach(url => (new Image().src = url))
+
+  // See Javascript Modulo Bug for negative numbers
+  function moduloFix(a, n) {
+    return ((a % n) + n) % n
   }
+
+  function nextPic(operation) {
+    if (urls.length == 1) {
+      return
+    }
+    setShowCounter(true)
+    setTimeout(() => setShowCounter(false), 1000)
+    setLoading(true)
+    setCurrentPic(moduloFix((operation(currentPic)), urls.length))
+  }
+
+  useEffect(() => {
+    setTimeout(() => setShowCounter(false), 1000)
+  }, [url])
 
   return (
     <div className={carouselStyles.carousel}>
       <div
-        className={carouselStyles.arrowContainer}
+        className={`${carouselStyles.arrowContainer} ${disableClick}`}
         id={carouselStyles.prevArrow}
-        onClick={nextPic}
+        onClick={() => nextPic((c) => c - 1)}
       >
-        <MdChevronLeft size="2em" color="gray"/>
+        <MdChevronLeft size="2em" color="gray" />
       </div>
-      <img src={`https:${url}`} />
       <div
-        className={carouselStyles.arrowContainer}
-        id={carouselStyles.nextArrow}
-        onClick={nextPic}
+        className={
+          loading
+            ? carouselStyles.loadingMessage
+            : `${carouselStyles.loadingMessage} ${carouselStyles.noDisplay}`
+        }
       >
-        <MdChevronRight size="2em" color="gray"/>
+        Loading...
+      </div>
+      <img
+        className={loading ? carouselStyles.loading : ""}
+        src={`https:${url}`}
+        onLoad={() => setLoading(false)}
+      />
+      <div
+        className={`${carouselStyles.counter} ${
+          showCounter ? "" : carouselStyles.noOpacity
+        }`}
+      >
+        {currentPic + 1} / {urls.length}
+      </div>
+      <div
+        className={`${carouselStyles.arrowContainer} ${disableClick}`}
+        id={carouselStyles.nextArrow}
+        onClick={() => nextPic((c) => c + 1)}
+      >
+        <MdChevronRight size="2em" color="gray" />
       </div>
     </div>
   )
