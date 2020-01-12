65b0f80030e7305d1ee45b97ec00098495782372
Hannes Hertach <hanneshertach@gmail.com>
2020-01-12 21:57:05 +0000

    Implement new gallery

--- src/pages/fotos.js
+++ src/pages/fotos.js
@@ -3,62 +3,82 @@ import Layout from "../components/layout"
 import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 import photoStyles from "./fotos.module.css"
-import { MdClose } from 'react-icons/md'
 
 export default ({ data }) => {
-  const [selectedPicture, setSelectedPicture] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const [currentRubrik, setCurrentRubrik] = useState(null)
+
+  const rubriken = Array.from(
+    new Set(
+      data.allContentfulOffentlicheGallerie.edges.map(edge => edge.node.rubrik)
+    )
+  )
+
+  const rubrikToData = {}
+  rubriken.forEach(r => (rubrikToData[r] = []))
+
+  data.allContentfulOffentlicheGallerie.edges.forEach(edge => {
+    rubrikToData[edge.node.rubrik].push({
+      fluid: edge.node.vorschau.fluid,
+      title: edge.node.titel,
+      link: edge.node.link,
+    })
+  })
 
   return (
     <Layout>
       <div className={photoStyles.archiveContainer}>
-        <Link to='/gallerie'>Zugang zum Passwortgeschützten Fotoarchiv</Link>
+        <h1>Fotos</h1>
+        <span>
+          <Link to="/gallerie">Zugang zum Passwortgeschützten Fotoarchiv</Link>
+        </span>
       </div>
-      <div className={photoStyles.gridContainer}>
-        {data.allContentfulGallerieBild.nodes.map(node => {
-          return (
-            <div
-              className={photoStyles.gridItem}
-              key={node.bild.file.url}
-              onClick={() => {
-                setLoading(true)
-                setSelectedPicture(node.bild.file.url)
-              }}
-            >
-              <Img style={{ height: "100%" }} fluid={node.bild.fluid}></Img>
-            </div>
-          )
-        })}
-      </div>
-      {selectedPicture != null && (
-        <div className={photoStyles.dim} onClick={() => setSelectedPicture(null)}>
-          <MdClose className={photoStyles.closeIcon} size='2em' />
-          <div className={photoStyles.pictureContainer}>
-            { loading && <div className={photoStyles.loader}>Loading...</div>}
-            <img
-              style={loading ? { display: "none" } : {}}
-              src={`https:${selectedPicture}`}
-              onLoad={() => setLoading(false)}
-              alt="picture"
-            ></img>
-          </div>
+      <div className={photoStyles.mainContainer}>
+        <div className={photoStyles.sidebar}>
+          <h3>Kategorien</h3>
+          <ul>
+            {rubriken.map(r => (
+              <li key={r} onClick={() => setCurrentRubrik(r)}>
+                {r}
+              </li>
+            ))}
+          </ul>
         </div>
-      )}
+        <div className={photoStyles.galleries}>
+          {currentRubrik &&
+            rubrikToData[currentRubrik].map(d => (
+              <div key={d.title} className={photoStyles.gallery}>
+                <a href={d.link}>
+                  <Img fluid={d.fluid} />
+                  <div className={photoStyles.title}>{d.title}</div>
+                </a>
+              </div>
+            ))}
+        </div>
+      </div>
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    allContentfulGallerieBild {
-      nodes {
-        bild {
-          fluid {
-            ...GatsbyContentfulFluid_tracedSVG
+    allContentfulOffentlicheGallerie {
+      edges {
+        node {
+          rubrik
+          vorschau {
+            fluid {
+              src
+              sizes
+              tracedSVG
+              aspectRatio
+              base64
+              srcSet
+              srcSetWebp
+              srcWebp
+            }
           }
-          file {
-            url
-          }
+          titel
+          link
         }
       }
     }
