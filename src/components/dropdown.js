f0bc93e18d5a4079b8a475ae3be374036ef82ab2
Hannes Hertach <hanneshertach@gmail.com>
2020-01-15 19:02:17 +0000

    Add mobile navigation

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,11 +1,15 @@
 import dropdownStyles from "./dropdown.module.css"
 import React, { useState } from "react"
 
-export default ({ title, children, titleClassName }) => {
+export default ({ title, children, titleClassName, disabled }) => {
   const [isCollapsed, setCollapse] = useState(true)
 
   function toggleCollapse() {
     setCollapse(!isCollapsed)
+  }
+
+  if (disabled) {
+    return <div className={dropdownStyles.disabled}>{children}</div>
   }
 
   return (
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,17 @@ import navbarStyles from "./navbar.module.css"
 import { Link, graphql, StaticQuery } from "gatsby"
 import Dropdown from "../components/dropdown"
 import Img from "gatsby-image"
+import { MdMenu, MdClose } from "react-icons/md"
 
 export default ({ children }) => {
   const [hasScrolled, setHasScrolled] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [isMobile, setIsMobile] = useState(false)
 
+  // add scroll listener for navbar shrink effect
   useEffect(() => {
     const handleScroll = () => {
-      setHasScrolled(window.scrollY > 0);
+      setHasScrolled(window.scrollY > 0)
     }
     window.addEventListener("scroll", handleScroll)
 
@@ -17,6 +21,32 @@ export default ({ children }) => {
       window.removeEventListener("scroll", handleScroll)
     }
   })
+
+  // add resize listener to (dis/en)able hamburger menu
+  useEffect(() => {
+    function handleResize() {
+      const m = window.matchMedia("only screen and (max-width: 800px)")
+      if (m.matches) {
+        setIsMobile(true)
+      } else {
+        setIsMobile(false)
+      }
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  })
+
+  function triggerMobileMenu() {
+    setMenuOpen(!menuOpen)
+  }
+
+  const menuStyle = menuOpen
+    ? `${navbarStyles.rightContainer} ${navbarStyles.open}`
+    : `${navbarStyles.rightContainer} ${navbarStyles.closed}`
 
   return (
     <StaticQuery
@@ -39,14 +69,17 @@ export default ({ children }) => {
               hasScrolled
                 ? {
                     backgroundColor: "white",
-                    boxShadow: '0px 7px 14px -4px rgba(99,99,99,0.1)'
+                    boxShadow: "0px 7px 14px -4px rgba(99,99,99,0.1)",
                   }
                 : {
                     background: "none",
-                    boxShadow: 'none'
+                    boxShadow: "none",
                   }
             }
           >
+            <div className={navbarStyles.hamburger} onClick={triggerMobileMenu}>
+              <MdMenu size={hasScrolled ? "1.6em" : "2em"} />
+            </div>
             <div className={navbarStyles.leftContainer}>
               <Link to="/">
                 <div
@@ -67,11 +100,14 @@ export default ({ children }) => {
                 </div>
               </Link>
             </div>
-            <div className={navbarStyles.rightContainer}>
+            <div className={menuStyle}>
+              <div className={navbarStyles.exit} onClick={triggerMobileMenu}>
+                <MdClose size="1.6em"/>
+              </div>
               <Link className={navbarStyles.underline} to="/about">
                 Über Pfadi
               </Link>
-              <Dropdown titleClassName={navbarStyles.underline} title="Stufen">
+              <Dropdown titleClassName={navbarStyles.underline} title="Stufen" disabled={isMobile}>
                 <Link className={navbarStyles.underline} to="/stufen/biber">
                   Biber
                 </Link>
