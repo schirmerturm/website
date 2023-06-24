014949a21d46a507635121af30b7ce77e8fd5b4f
Hannes Hertach <hanneshertach@gmail.com>
2023-06-24 20:31:16 +0200

    Revert "remove analytics"
    This reverts commit 52a0498de83ce55fa1c6e3b7cc5010da385d5ad2.

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,23 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-156294066-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: false,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
